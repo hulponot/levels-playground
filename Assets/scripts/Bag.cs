ead192668d27cc3fb27935d91c6861e937260cec
hulponot <hulponot@gmail.com>
2018-02-13 00:24:17 +0300

    collectable color cubes

---
+++ Assets/scripts/Bag.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bag : MonoBehaviour {
+    public enum ColorEnum { Red = 0, Yellow = 1, Blue = 2 };
+
+    public Text textArea;
+    private List<ICollactable> content;
+
+    private void Start()
+    {
+        content = new List<ICollactable>();
+    }
+
+    public IEnumerable<String> GetStringsContent()
+    {
+        return content.Select( c => c.GetName());
+    }
+
+    public void PlaceInBag(ICollactable thing)
+    {
+        if (!content.Contains(thing))
+        {
+            content.Add(thing);
+            UpdateText();
+        }
+    }
+
+    public void UpdateText()
+    {
+        textArea.text = "Items collected: " + String.Join(", ", GetStringsContent().ToArray());
+            
+    }
+}
---
+++ Assets/scripts/CollectableColorBox.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.scripts.Interfaces;
+
+public class CollectableColorBox : MonoBehaviour, ICollactable
+{
+    public Bag bag;
+    public Bag.ColorEnum color;
+    public List<Material> colorMaterials;
+
+    private void Start()
+    {
+        SetColorMaterial();
+
+        StartCoroutine(BoxRotation());
+    }
+    public void Collect()
+    {
+        bag.PlaceInBag(this);
+        gameObject.SetActive(false);
+    }
+    public string GetName()
+    {
+        return color.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Collect();
+    }
+  
+    private void SetColorMaterial()
+    {
+        var render = GetComponent<Renderer>();
+        render.material = colorMaterials[(int)color];
+    }
+
+    public IEnumerator BoxRotation()
+    {
+        while (isActiveAndEnabled)
+        {
+            transform.Rotate(new Vector3(0.2f, 0.1f, 0.5f));
+            yield return null;
+        }
+        yield break;
+    }
+}
